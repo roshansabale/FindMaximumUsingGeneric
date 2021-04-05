f3a0daed88d09b309e8cb948a829f24fb10238ba
roshansabale@yahoo.com <roshansabale@yahoo.com>
2021-04-05 17:47:19 +0530

    Added:Functionality to check second number as maximum

--- src/test/java/FindMaximumTest.java
+++ src/test/java/FindMaximumTest.java
@@ -2,14 +2,26 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 public class FindMaximumTest {
+    Integer number1;
+    Integer number2;
+    Integer number3;
+    Integer integerMaximum;
+    FindMaximum findMaximum = new FindMaximum();
     @Test
     void findMaximumNumber_WhenAtPosition1_ThenReturnNumber1() {
-        Integer number1=10;
-        Integer number2=5;
-        Integer number3=9;
-
-        FindMaximum findMaximum = new FindMaximum();
-        Integer integerMaximum = findMaximum.findIntegerMaximum(10, 5, 9);
+        number1 = 10;
+        number2 = 5;
+        number3 = 9;
+        integerMaximum = findMaximum.findIntegerMaximum(10, 5, 9);
         Assert.assertEquals(number1,integerMaximum);
+    }
+
+    @Test
+    void findMaximumNumber_WhenAtPosition1_ThenReturnNumber2() {
+        number1 = 5;
+        number2 = 10;
+        number3 = 2;
+        integerMaximum = findMaximum.findIntegerMaximum(number1,number2,number3);
+        Assert.assertEquals(number2,integerMaximum);
     }
 }
