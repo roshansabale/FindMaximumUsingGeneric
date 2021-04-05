a34941c5815e626237f15c4566fe0f83bdd1250c
roshansabale@yahoo.com <roshansabale@yahoo.com>
2021-04-05 17:31:09 +0530

    Added:Functionality to check first number as a maximum

--- src/main/java/FindMaximum.java
+++ src/main/java/FindMaximum.java
@@ -1,2 +1,11 @@
 public class FindMaximum {
+    public Integer findIntegerMaximum(Integer number1,Integer number2,Integer number3) {
+        Integer maximum=number1;
+        if(number2.compareTo(maximum) > 0) {
+            maximum=number2;
+        } else if (number3.compareTo(maximum) > 0) {
+            maximum=number3;
+        }
+        return maximum;
+    }
 }
--- src/test/java/FindMaximumTest.java
+++ src/test/java/FindMaximumTest.java
@@ -1,2 +1,15 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
 public class FindMaximumTest {
+    @Test
+    void findMaximumNumber_WhenAtPosition1_ThenReturnNumber1() {
+        Integer number1=10;
+        Integer number2=5;
+        Integer number3=9;
+
+        FindMaximum findMaximum = new FindMaximum();
+        Integer integerMaximum = findMaximum.findIntegerMaximum(10, 5, 9);
+        Assert.assertEquals(number1,integerMaximum);
+    }
 }
