3772f140ee8cbdf77a4ce61625ac05ed1226fc91
roshansabale@yahoo.com <roshansabale@yahoo.com>
2021-04-05 18:10:28 +0530

    Refactored: modify findmaximum method and add code to check maximum number at position 3

--- src/main/java/FindMaximum.java
+++ src/main/java/FindMaximum.java
@@ -1,10 +1,14 @@
 public class FindMaximum {
     public Integer findIntegerMaximum(Integer number1,Integer number2,Integer number3) {
         Integer maximum=number1;
-        if(number2.compareTo(maximum) > 0) {
-            maximum=number2;
-        } else if (number3.compareTo(maximum) > 0) {
-            maximum=number3;
+
+        if(number1.compareTo(number2) > 0) {
+            maximum = number1;
+        } else if (number2.compareTo(number3) > 0) {
+            maximum =number2;
+        } else if (number3.compareTo(number1) > 0)
+        {
+            maximum = number3;
         }
         return maximum;
     }
--- src/test/java/FindMaximumTest.java
+++ src/test/java/FindMaximumTest.java
@@ -14,14 +14,26 @@ public class FindMaximumTest {
         number3 = 9;
         integerMaximum = findMaximum.findIntegerMaximum(10, 5, 9);
         Assert.assertEquals(number1,integerMaximum);
+        System.out.println("Maximum at 1rd position Number is:"+integerMaximum);
     }
 
     @Test
-    void findMaximumNumber_WhenAtPosition1_ThenReturnNumber2() {
+    void findMaximumNumber_WhenAtPosition2_ThenReturnNumber2() {
         number1 = 5;
         number2 = 10;
         number3 = 2;
         integerMaximum = findMaximum.findIntegerMaximum(number1,number2,number3);
         Assert.assertEquals(number2,integerMaximum);
+        System.out.println("Maximum at 2nd position Number is:"+integerMaximum);
+    }
+
+    @Test
+    void findMaximumNumber_WhenAtPosition3_ThenReturnNumber3() {
+        number1 = 10;
+        number2 = 20;
+        number3 = 30;
+        integerMaximum = findMaximum.findIntegerMaximum(number1,number2,number3);
+        Assert.assertEquals(number3,integerMaximum);
+        System.out.println("Maximum at 3rd position Number is:"+integerMaximum);
     }
 }
